ee73d1a52e8a4062b07f9443aec76ebd406e1571
The__Glacier <149191600+PetitGlacon@users.noreply.github.com>
2025-10-26 15:21:49 +0100

    1.0.0 SNAPSHOT 1
    Version 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -73,6 +73,10 @@ publishing {
             groupId = "fr.The__Glacier"
             artifactId = "G-Worlds"
             version = "1.0.0-SNAPSHOT"
+
+            artifact(tasks["shadowJar"]) {
+                classifier = ""  // Pas de classifier pour éviter des artefacts multiples
+            }
         }
     }
     repositories {
